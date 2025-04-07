db5b64a2c03d4e34cccb9323445c719da8b3b174
Anran Zhang <anran.zhang@tum.de>
2025-04-07 15:35:46 +0200

    minor

--- benchmark/exp_summarizer.py
+++ benchmark/exp_summarizer.py
@@ -17,13 +17,17 @@ def save_pickle(pickle_file, data):
     with open(pickle_file, 'wb') as pfile:
         pickle.dump(data, pfile)
 
-exp_results_fp = './outputs/CloseMicrowave/ours/exp_results_all.pkl'
-exp_results = load_pickle(exp_results_fp)
-exp_summarized = {}
-for camera_name in exp_results.keys():
-    exp_result = exp_results[camera_name]
-    exp_summarized[camera_name] = np.mean(exp_result, axis=0)
-    exp_results[camera_name] = exp_result
-exp_summarized['all'] = np.mean(list(exp_summarized.values()), axis=0)
-print(exp_summarized)
-breakpoint()
+import argparse
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_results_fp', type=str, default='outputs/OpenMicrowave/ours/trial_2025-04-06_20-20/exp_results/exp_results_all.pkl')
+    args = parser.parse_args()
+    exp_results_fp = args.exp_results_fp
+    exp_results = load_pickle(exp_results_fp)
+    exp_summarized = {}
+    for camera_name in exp_results.keys():
+        exp_result = exp_results[camera_name]
+        exp_summarized[camera_name] = np.mean(exp_result, axis=0)
+        exp_results[camera_name] = exp_result
+    exp_summarized['all'] = np.mean(list(exp_summarized.values()), axis=0)
+    print(exp_summarized)
