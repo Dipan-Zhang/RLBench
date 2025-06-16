453e4051b37249a5f9ac88461c981c52c51616b6
Anran Zhang <anran.zhang@tum.de>
2025-06-16 19:42:13 +0200

    update benchmark script for ablations

--- run_benchmark.py
+++ run_benchmark.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('--trial_dir', type=str, help='batch eval using same trial directory')
     parser.add_argument('--headless', action='store_true', help='run in headless mode')
     parser.add_argument('--DEBUG_VIS', action='store_true', help='visualize the motion in rlbench')
+    parser.add_argument('--num_var', type=int, default=0, help='use variation')
 
     args = parser.parse_args()
 
@@ -23,26 +24,57 @@ if __name__ == "__main__":
     TASK_LIST_ARTICULATE = sim_cfg['ARTICULATE_TASK_LIST']
 
     if args.task == 'all':
-        TASKS = TASK_LIST_PORTABLE + TASK_LIST_ARTICULATE
+        TASKS =  TASK_LIST_ARTICULATE + TASK_LIST_PORTABLE
     elif args.task == 'portable':
         TASKS = TASK_LIST_PORTABLE
     elif args.task == 'articulate':
         TASKS = TASK_LIST_ARTICULATE
     elif args.task == 'flex':
-        TASKS = ['close_cabinet', 'close_laptop', 'open_dishwasher', 'down_toilet_seat', 'open_']
+        TASKS = ['down_toilet_seat', 'close_laptop']
+    elif args.task == 'bechmarkv1_rest':
+        TASKS = ['open_drawer', 'close_microwave', 'open_slide_cabinet', 'close_slide_cabinet', 'close_cabinet']
+    elif args.task == 'ablation_multiple_goal_rest':
+        TASKS = ['open_drawer', 'open_dishwasher', 'down_toilet_seat', 'close_slide_cabinet', 'open_slide_cabinet']
+        
+    elif args.task =='ablation_2D':
+        TASKS = [
+            'open_microwave', 
+            'close_microwave',
+            'open_drawer', 
+            'open_dishwasher',
+            'open_slide_cabinet',
+            'close_laptop',
+            'open_cabinet',
+        ]
     else:
         TASKS = [args.task]
 
+    if args.task == 'ablation_var':
+        SAVE_BASE_DIR = '../RLBench/outputs_ablation_var'
+    elif args.task == 'ablation_2D':
+        SAVE_BASE_DIR = '../RLBench/outputs_ablation_2D'
+    else:
+        SAVE_BASE_DIR = '../RLBench/outputs'
+
     for task in tqdm(TASKS):
-        # print(f"====================Task: {task}=====================")
-        # # task_env, task_name = task.split("@")
+        print(f"====================Task: {task}=====================")
         taskName = underscore_string_to_camel_case(task)
-        trial_dir = os.path.join('./outputs', taskName, method, args.trial_dir)
-        cmd = f"python benchmark/test_affordance.py --task_name {task} --method {method} --trial_dir {trial_dir}"
-        if args.headless:
-            cmd += " --headless"
-        if args.DEBUG_VIS:
-            cmd += " --DEBUG_VIS"
-        os.system(cmd)
+        if args.num_var == 0:
+            trial_dir = os.path.join(SAVE_BASE_DIR, taskName, method, args.trial_dir)
+            cmd = f"python benchmark/test_affordance.py --task_name {task} --method {method} --trial_dir {trial_dir}"
+            if args.headless:
+                cmd += " --headless"
+            if args.DEBUG_VIS:
+                cmd += " --DEBUG_VIS"
+            os.system(cmd)
+        else:
+            for var in range(args.num_var):
+                trial_dir = os.path.join(SAVE_BASE_DIR, taskName, method, args.trial_dir, f'var_{var}')
+                cmd = f"python benchmark/test_affordance.py --task_name {task} --method {method} --trial_dir {trial_dir}"
+                if args.headless:
+                    cmd += " --headless"
+                if args.DEBUG_VIS:
+                    cmd += " --DEBUG_VIS"
+                os.system(cmd)
 
         print("=======================================================")
